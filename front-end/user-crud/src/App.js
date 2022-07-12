ae04238be48df61b7f99bab6fc0dcb265958e59c
aselwilliams <aselwilliams22@gmail.com>
2022-07-12 08:57:50 -0400

    navigation is added

--- front-end/user-crud/src/App.js
+++ front-end/user-crud/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './Home'
-import AddNewUser from './AddNewUser'
+import Home from './Home';
+import AddNewUser from './AddNewUser';
+import Navigation from './Navigation';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="container">
       <div className='row'>
         <BrowserRouter>
+        <Navigation />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/addNewUser' element={<AddNewUser />}/>
